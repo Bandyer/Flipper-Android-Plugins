2f402dd0dce6d071825f9d349f8f9e4a5de76915
Kristiyan Petrov <kristiyan.petrov@bandyer.com>
2022-02-14 16:28:25 +0100

    first commit

---
+++ flipper-socket-io-plugin/src/main/java/com/bandyer/flipper_socket_io_plugin/FlipperOKHttpClient.kt
@@ -0,0 +1,90 @@
+package com.bandyer.flipper_socket_io_plugin
+
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okio.ByteString
+
+class FlipperOKHttpClient(
+    private val SIONetworkFlipperPlugin: SIONetworkFlipperPlugin,
+    okHttpClient: OkHttpClient = OkHttpClient()
+) : OkHttpClient() {
+
+    private var client: OkHttpClient = okHttpClient.newBuilder().addInterceptor(FlipperOkhttpInterceptor(SIONetworkFlipperPlugin.networkFlipperPlugin)).build()
+
+    override fun newCall(request: Request): Call = client.newCall(request)
+
+    override fun newWebSocket(request: Request, listener: WebSocketListener): WebSocket {
+
+        val url = request.url.toUrl().toString()
+
+        return StethoWebSocket(
+            client.newWebSocket(request, object : WebSocketListener() {
+
+                override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                    super.onClosed(webSocket, code, reason)
+                    listener.onClosed(webSocket, code, reason)
+                    SIONetworkFlipperPlugin.emit(url, "socket closed - ${reason.takeIf { it.isNotBlank() } ?: code}")
+                    SIONetworkFlipperPlugin.clearEvents(url)
+                }
+
+                override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                    super.onClosing(webSocket, code, reason)
+                    listener.onClosing(webSocket, code, reason)
+                }
+
+                override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                    super.onFailure(webSocket, t, response)
+                    listener.onFailure(webSocket, t, response)
+                    SIONetworkFlipperPlugin.emit(url, t.message ?: "socket failed")
+                }
+
+                override fun onMessage(webSocket: WebSocket, text: String) {
+                    super.onMessage(webSocket, text)
+                    listener.onMessage(webSocket, text)
+                    SIONetworkFlipperPlugin.on(url, text)
+                }
+
+                override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                    super.onMessage(webSocket, bytes)
+                    listener.onMessage(webSocket, bytes)
+                    SIONetworkFlipperPlugin.on(url, bytes.string(SIONetworkFlipperPlugin.charset))
+                }
+
+                override fun onOpen(webSocket: WebSocket, response: Response) {
+                    super.onOpen(webSocket, response)
+                    listener.onOpen(webSocket, response)
+                    SIONetworkFlipperPlugin.emit(url, response.message)
+                }
+            }),
+            SIONetworkFlipperPlugin
+        )
+    }
+
+    private class StethoWebSocket(private val wrappedSocket: WebSocket, private val SIONetworkFlipperPlugin: SIONetworkFlipperPlugin) : WebSocket {
+
+        private val url = request().url.toUrl().toString()
+
+        override fun queueSize() = wrappedSocket.queueSize()
+
+        override fun send(text: String): Boolean {
+            SIONetworkFlipperPlugin.emit(url, text)
+            return wrappedSocket.send(text)
+        }
+
+        override fun send(bytes: ByteString): Boolean {
+            SIONetworkFlipperPlugin.emit(url, bytes.string(SIONetworkFlipperPlugin.charset))
+            return wrappedSocket.send(bytes)
+        }
+
+        override fun close(code: Int, reason: String?) = wrappedSocket.close(code, reason)
+
+        override fun cancel() = wrappedSocket.cancel()
+
+        override fun request() = wrappedSocket.request()
+    }
+}
---
+++ flipper-socket-io-plugin/src/main/java/com/bandyer/flipper_socket_io_plugin/SIONetworkFlipperPlugin.kt
@@ -0,0 +1,113 @@
+package com.bandyer.flipper_socket_io_plugin
+
+import android.content.Context
+import com.bandyer.flipper_socket_io_plugin.SIONetworkFlipperPlugin.SocketEvent.Disconnect
+import com.bandyer.flipper_socket_io_plugin.SIONetworkFlipperPlugin.SocketEvent.Emit
+import com.bandyer.flipper_socket_io_plugin.SIONetworkFlipperPlugin.SocketEvent.On
+import com.bandyer.flipper_socket_io_plugin.SIONetworkFlipperPlugin.SocketEvent.Ping
+import com.bandyer.flipper_socket_io_plugin.SIONetworkFlipperPlugin.SocketEvent.Pong
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.core.FlipperArray
+import com.facebook.flipper.core.FlipperConnection
+import com.facebook.flipper.core.FlipperObject
+import com.facebook.flipper.core.FlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import org.json.JSONArray
+import org.json.JSONObject
+import java.nio.charset.Charset
+import java.util.UUID
+
+class SIONetworkFlipperPlugin(context: Context) : FlipperPlugin {
+
+    val networkFlipperPlugin = NetworkFlipperPlugin().apply {
+        AndroidFlipperClient.getInstance(context).addPlugin(this)
+    }
+
+    override fun getId(): String = "SocketIO"
+
+    private var connection: FlipperConnection? = null
+
+    private var socketEvents: MutableMap<String, MutableList<SocketEvent>> = mutableMapOf()
+
+    var charset = Charset.defaultCharset()
+
+    sealed class SocketEvent(val body: String, val type: Type) {
+
+        private val id: Int = UUID.randomUUID().toString().hashCode()
+
+        val asJsonObject: JSONObject = JSONObject()
+            .put("id", id)
+            .put("body", body.replace("^[- 0-9]*".toRegex(), "").takeIf { it.isNotBlank() } ?: body)
+            .put("type", type.value)
+
+        enum class Type(val value: String) {
+            Send("emit"),
+            Receive("on")
+        }
+
+        object Ping : SocketEvent(body = "ping", type = Type.Receive)
+        class On(body: String) : SocketEvent(body, type = Type.Receive)
+
+        object Open : SocketEvent(body = "socket-open", type = Type.Send)
+        object Disconnect : SocketEvent(body = "socket disconnected", type = Type.Send)
+        object Pong : SocketEvent(body = "pong", type = Type.Send)
+        class Emit(body: String) : SocketEvent(body, type = Type.Send)
+    }
+
+    @Synchronized
+    fun emit(url: String, text: String) {
+        val event = when (text) {
+            "41" -> Disconnect
+            "40" -> SocketEvent.Open
+            "3"  -> Pong
+            else -> Emit(text)
+        }
+
+        if (socketEvents[url].isNullOrEmpty()) socketEvents[url] = mutableListOf(event)
+        else socketEvents[url]!!.add(event)
+
+        updateEvents(url, socketEvents[url]!!)
+    }
+
+    @Synchronized
+    fun on(url: String, text: String) {
+        val event = when (text) {
+            "2"  -> Ping
+            else -> On(text)
+        }
+
+        if (socketEvents[url].isNullOrEmpty()) socketEvents[url] = mutableListOf(event)
+        else socketEvents[url]!!.add(event)
+
+        updateEvents(url, socketEvents[url]!!)
+    }
+
+    override fun onConnect(connection: FlipperConnection?) {
+        this.connection = connection
+    }
+
+    private fun updateEvents(url: String, events: List<SocketEvent>) {
+        val payload = JSONArray().apply { events.forEach { put(it.asJsonObject) } }.toString()
+
+        val row = FlipperObject.Builder()
+            .put("id", url.hashCode())
+            .put("url", url)
+            .put("events", FlipperArray(payload))
+            .build()
+
+        connection?.send("newSocket", row)
+    }
+
+    @Synchronized
+    override fun onDisconnect() {
+        connection = null
+        socketEvents.clear()
+    }
+
+    override fun runInBackground(): Boolean = true
+
+    @Synchronized
+    fun clearEvents(url: String) {
+        socketEvents[url]!!.clear()
+    }
+}
